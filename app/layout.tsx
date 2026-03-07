a8884732e6ec869a2422664f0ed9151477ae82b0
Solimany04 <aabkrino12@gmail.com>
2026-03-07 19:03:07 +0200

    fixing logo compatibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  icons:"/Vector.svg",
+  icons:"/vector.svg",
   title: "Tbarak",
   description: "Tbarak for high quality textiles",
 };
