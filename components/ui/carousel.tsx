1898ca346757466849e69f4c5816a93f51f1a58d
Solimany04 <aabkrino12@gmail.com>
2026-05-13 19:06:57 +0300

    chore(ui): adjust carousel and input primitives

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -185,7 +185,7 @@ function CarouselPrevious({
       variant={variant}
       size={size}
       className={cn(
-        "absolute size-8 rounded-full",
+        "absolute size-10.5 rounded-full bg-accent/5 backdrop-blur-sm",
         orientation === "horizontal"
           ? "top-1/2 -start-12 -translate-y-1/2"
           : "-top-12 start-1/2 -translate-x-1/2 rtl:translate-x-1/2 rotate-90",
@@ -195,7 +195,7 @@ function CarouselPrevious({
       onClick={scrollPrev}
       {...props}
     >
-      <ArrowLeft />
+      <ArrowLeft className="size-7 text-white" />
       <span className="sr-only">Previous slide</span>
     </Button>
   )
@@ -215,7 +215,7 @@ function CarouselNext({
       variant={variant}
       size={size}
       className={cn(
-        "absolute size-8 rounded-full",
+        "absolute size-10.5 rounded-full bg-accent/5 backdrop-blur-sm",
         orientation === "horizontal"
           ? "top-1/2 -end-12 -translate-y-1/2"
           : "-bottom-12 start-1/2 -translate-x-1/2 rtl:translate-x-1/2 rotate-90",
@@ -225,7 +225,7 @@ function CarouselNext({
       onClick={scrollNext}
       {...props}
     >
-      <ArrowRight />
+      <ArrowRight className="size-7 text-white" />
       <span className="sr-only">Next slide</span>
     </Button>
   )
